8497386b4ea15949383c4bface7c25cdc01d0c4f
M1sm4tch <91552196+M1sm4tch@users.noreply.github.com>
2024-02-02 15:59:42 +0530

    front-end complete

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between">
-        
+      <div className="container pt-0 p-12 flex justify-between items-center">
+        <Image src="/logo.svg" alt="logo" width={150} height={150} className='mt-0'/>
         <p className="text-slate-600">All rights reserved.</p>
       </div>
     </footer>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -46,7 +46,7 @@ const Navbar = () => {
                 }
             </div>
             <div className='menu hidden md:block md:w-auto' id="navbar">
-                <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+                <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0 mr-5">
                     {navLinks.map((link,index)=>{
                         return(
                             <li key={index}>
